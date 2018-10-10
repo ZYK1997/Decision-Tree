07053009a1f41a76e8289b0a284f3ec5c1c97fa6
zhuyk6 <zhuyk6@gmail.com>
2018-10-10 02:48:32 +0000

    modified README.md and DecisionTree.jl

--- DecisionTree.jl
+++ DecisionTree.jl
@@ -5,25 +5,60 @@ decision tree
 using BenchmarkTools
 
 import CSV
-
+import DataFrames
 import Random
+import Plots
 
+"""
+	Leaf
+
+Define the leaf of the decision tree, `Leaf` contains the label.
+"""
 struct Leaf
     label::Int
 end
 
+"""
+	Node
+
+Define the node of the decision tree, `Node` contains the attribute it chooses.
+"""
 struct Node
     attribute::Int
     son::Vector{Union{Leaf, Node}}
 end
 
+function showTree(io::IO, t::Leaf, dep::Int, str::String = "")
+	println(io, "\t"^dep * str * "Leaf: label: $(t.label)")
+end
+
+function showTree(io::IO, t::Node, dep::Int, str::String = "")
+	println(io, "\t"^dep * str * "Node: attribute: $(t.attribute)")
+	for (i, son) in enumerate(t.son)
+		showTree(io, son, dep + 1, "son$i ")
+	end
+end
+
 # dump(Node(1, [Node(2, [Leaf(1), Leaf(2)]), Leaf(2)]))
 
 # v should be Array{Int, 2}, every attribute should be Int
-getLabel(t::Leaf, v) = t.label
+"""
+	getLabel(t::Union{Leaf, Node}, v)
 
+`getLabel` is the function to get one vector's label, it goes from the root to
+the Leaf and return the leaf's label.
+"""
+getLabel(t::Leaf, v) = t.label
 getLabel(t::Node, v) = getLabel(t.son[v[t.attribute]], v)
 
+"""
+	splitData(data, attribute, numOfClasses)
+
+It is the function to split `data` according to the `attribute`.
+`data` is the vector of vectors(examples),
+`attribute` is the chosen attribute's id, it is an integer,
+`numOfClasses` is the chosen attribute's classes.
+"""
 function splitData(data, attribute::Int, numOfClasses::Int)
     map(1:numOfClasses) do i
         collect(v for v in data if v[attribute] == i)
@@ -37,6 +72,16 @@ data = [[2, 1], [3,1], [4, 1], [5, 2]]
 all(v[end] == data[1][end] for v in data)
 =#
 
+"""
+	build(data, attributes, findAttribute, labelSize)
+
+It is the function to build the decision tree.
+`data` is the vector of vectors(examples),
+`attributes` is the set of attributes' id which can be used,
+`findAttribute` is the function to choose the attribute, it depends on the
+model you choose,
+`labelSize` is the vector of number of attributes' classes.
+"""
 function build(
     data::Vector{Vector{Int}},
     attributes::Set{Int},
@@ -44,6 +89,7 @@ function build(
     labelSize::Vector{Int}
     )::Union{Leaf, Node}
 
+	# Here are three kinds of bound situations.
     if isempty(data)
         return Leaf(0)
     elseif isempty(attributes)
@@ -64,6 +110,14 @@ function build(
     Node(attr, sons)
 end
 
+
+"""
+	countLabel(data, attr, num)
+
+It is the function to split `data` according to attribute `attr` and count
+the number of every classes.
+The result is the frequency of every classes.
+"""
 function countLabel(data, attr::Int, num::Int)
     if size(data)[1] == 0
         return zeros(num)
@@ -72,7 +126,7 @@ function countLabel(data, attr::Int, num::Int)
     for v in data
         sumup[v[attr]] += 1
     end
-    sumup ./ size(data)[1]
+    sumup ./ length(data)
 end
 
 #=
@@ -80,9 +134,14 @@ data = [[1, 2, 3], [2, 1, 1], [2, 1, 1], [2, 3, 2]]
 countLabel(data, 3, 3)
 =#
 
+"""
+	findFunction_ID3(data, attributes, labelSize)
+
+It is the function to choose the attribute from the set `attributes`.
+"""
 function findFunction_ID3(data, attributes, labelSize)::Int
     H(p) = iszero(p) ? 0.0 : -p * log2(p)
-    (m, ) = size(data[1])
+    m = length(labelSize)
 
     H_D = sum(H, countLabel(data, m, labelSize[m])) # H(D)
     H_D_A = map(attr for attr in attributes) do attr
@@ -96,9 +155,14 @@ function findFunction_ID3(data, attributes, labelSize)::Int
     minimum(H_D_A)[2]
 end
 
+"""
+	findFunction_C45(data, attributes, labelSize)
+
+It is the function to choose the attribute from the set `attributes`.
+"""
 function findFunction_C45(data, attributes, labelSize)::Int
     H(p) = iszero(p) ? 0.0 : -p * log2(p)
-    (m, ) = size(data[1])
+    m = length(labelSize)
 
     H_D = sum(H, countLabel(data, m, labelSize[m])) # H(D)
     H_A   = map(attr for attr in attributes) do attr
@@ -116,9 +180,14 @@ function findFunction_C45(data, attributes, labelSize)::Int
     maximum(gain_ratio)[2]
 end
 
+"""
+	findFunction_CART(data, attributes, labelSize)
+
+It is the function to choose the attribute from the set `attributes`.
+"""
 function findFunction_CART(data, attributes, labelSize)::Int
 	gini(v) = sum(p -> p * (1 - p), v)
-	(m, ) = size(data[1])
+	m = length(labelSize)
 
 	g_D = gini(countLabel(data, m, labelSize[m]))
 	g_D_A = map(attr for attr in attributes) do attr
@@ -132,8 +201,13 @@ function findFunction_CART(data, attributes, labelSize)::Int
 	minimum(g_D_A)[2]
 end
 
-train_set = CSV.read(pwd() * "\\lab2_data\\Car_train.csv")
+train_set = CSV.read(pwd() * "/data/Car_train.csv")
 
+"""
+	getDict(dataset)
+
+It is the function to get the dictionaries which map the attributes to integer.
+"""
 function getDict(dataset)
     map(1:7) do j
         T = typeof(dataset[j][1])
@@ -151,11 +225,17 @@ end
 
 # getDict(train_set)
 
+"""
+	transformDataSet(dataset, dictionaries)
+
+It is the function to transform the dataset to vectors of vectors(examples)
+	of integers according to dictionaries.
+"""
 function transformDataSet(dataset, dictionaries)
     ys = map(1:7) do j
         dict = dictionaries[j]
         map(dataset[j]) do w
-            dict[w]
+            haskey(dict, w) ? dict[w] : -1
         end
     end
     (n, ) = size(ys[1])
@@ -164,7 +244,12 @@ end
 
 # transformDataSet(train_set, getDict(train_set))
 
-function k_fold_crossValidation(
+"""
+	k_fold_cross_validation(K, dataset, dictionaries, labelSize, findFunction)
+
+It is the function to get the accuracy of the model.
+"""
+function k_fold_cross_validation(
     K::Int,
     dataset, dictionaries, labelSize,
     findFunction
@@ -192,9 +277,66 @@ end
 dictionaries = getDict(train_set)
 labelSize = map(d -> length(d), dictionaries)
 dataset = transformDataSet(train_set, dictionaries)
-k_fold_crossValidation(10, dataset, dictionaries, labelSize, findFunction_ID3)
-k_fold_crossValidation(10, dataset, dictionaries, labelSize, findFunction_C45)
-k_fold_crossValidation(10, dataset, dictionaries, labelSize, findFunction_CART)
+
+# show the three kinds of trees
+begin
+	tree1 = build(dataset, Set(1:length(labelSize) - 1), findFunction_ID3, labelSize)
+	open("ID3_Tree.txt", "w") do f
+		showTree(f, tree1, 0, "")
+	end
+	tree2 = build(dataset, Set(1:length(labelSize) - 1), findFunction_C45, labelSize)
+	open("C45_Tree.txt", "w") do f
+		showTree(f, tree2, 0, "")
+	end
+	tree3 = build(dataset, Set(1:length(labelSize) - 1), findFunction_CART, labelSize)
+	open("CART_Tree.txt", "w") do f
+		showTree(f, tree3, 0, "")
+	end
+end
+
+# choss the best model
+begin
+	line_K = 2:5
+	line_ID3 = zeros(Float64, length(line_K))
+	foreach(1:10) do _
+		line_ID3 .+= map(line_K) do k
+			k_fold_cross_validation(k, dataset, dictionaries, labelSize, findFunction_ID3)
+		end
+	end
+	line_ID3 = line_ID3 ./ 10
+	line_C45 = zeros(Float64, length(line_K))
+	foreach(1:10) do _
+		line_C45 .+= map(line_K) do k
+			k_fold_cross_validation(k, dataset, dictionaries, labelSize, findFunction_C45)
+		end
+	end
+	line_C45 = line_C45 ./ 10
+	line_CART = zeros(Float64, length(line_K))
+	foreach(1:10) do _
+		line_CART .+= map(line_K) do k
+			k_fold_cross_validation(k, dataset, dictionaries, labelSize, findFunction_CART)
+		end
+	end
+	line_CART = line_CART ./ 10
+	figure = Plots.plot(line_K, [line_ID3, line_C45, line_CART])
+	Plots.savefig(figure, "model_compare.png")
+end
+
+
+# label the test set
+begin
+	test_set = CSV.read(pwd() * "/data/Car_test.csv")
+	m = length(dictionaries)
+	tree = build(transformDataSet(train_set, dictionaries), Set(1:(m - 1)), findFunction_C45, labelSize)
+	predict = map(v -> getLabel(tree, v), transformDataSet(test_set, dictionaries))
+	reverseDict = collect(keys(dictionaries[m]))
+	result = map(predict) do v
+		iszero(v) ? rand(reverseDict) : reverseDict[v]
+	end
+	# @show reverseDict
+	df = DataFrames.DataFrame(label = result)
+	CSV.write("result.csv", df)
+end
 
 #=
 data = [
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Decision-Tree
+
+版本： `julia 1.0`
+依赖的包: `BenchmarkTools`, `CSV`, `Random`, `DataFrames`, `Plots`
+
+API：
+- 类型定义：
+    - `Leaf`
+    - `Node`
+    - `showTree` : 将树序列化`print`出来
+- 训练：
+    - `build` : 建树
+    - `findFunction_ID3` : ID3模型
+    - `findFunction_C45` : C4.5模型
+    - `findFunction_CART` : CART模型
+    - `k_fold_cross_validation` : $k$折交叉认证
+    - `getDict` : 将原数据标签映射为整数
+    - `transformDataSet` : 将原数据转换为向量的列表
+- 预测
+    - `getLabel` : 对一个向量预测其标签
+
+> 注：数据放在`/data/`里面，原有数据没有`head`，需要手动加上`head`，不然无法正确读取csv文件。
